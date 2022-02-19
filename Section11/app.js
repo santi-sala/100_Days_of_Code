0b27e2279e0d78503074470e80ed63681096d346
Santiago Sala <pipo0716@gmail.com>
2022-02-20 00:42:33 +0200

    230

---
+++ Section11/app.js
@@ -0,0 +1,18 @@
+let myName = "Pipo";
+let age = 78;
+let hobbies = ["Cooking", "Coding", "Snowboarding", "Climbing"];
+let job = {
+  title: "nurse",
+  place: "Laukaa",
+  salary: 60000,
+};
+
+// alert("Sup " + myName.toUpperCase());
+// alert(myName.toUpperCase() + " is " + age + " years old.");
+alert(
+  myName +
+    " is working as a " +
+    job.title +
+    ". One of his hobbies is: " +
+    hobbies[0]
+);
