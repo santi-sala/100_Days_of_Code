8f45b1431dbceaef15b6f2a39a319bbfc6834179
Santiago Sala <pipo0716@gmail.com>
2022-02-22 00:14:41 +0200

    266

--- Section 12/app.js
+++ Section 12/app.js
@@ -1,3 +1,8 @@
-document.body.children[1].children[0].href = "https://www.google.com";
+// document.body.children[1].children[0].href = "https://www.google.com";
 
 // console.dir(document);
+
+document.getElementById("external-link").href = "https://elotrolado.net";
+
+let anchorElement = document.querySelector("#external-link");
+anchorElement.href = "http://www.as.com";
