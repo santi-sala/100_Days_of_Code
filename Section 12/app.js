9e26933a9b832920c04aad4cfe540ff7e80d42bd
Santiago Sala <pipo0716@gmail.com>
2022-02-23 00:22:54 +0200

    272

--- Section 12/app.js
+++ Section 12/app.js
@@ -10,11 +10,11 @@ anchorElement.href = "http://www.as.com";
 // Add an Element
 // 1. Create a the new element
 let newAnchorElement = document.createElement("a");
-newAnchorElement.textContent = "new link!!";
+newAnchorElement.textContent = " sport.es!!";
 newAnchorElement.href = "https://www.sport.es";
 // 2. Get access to the parent elemnt that should hold the new element
-let firstParagraph =
-  document.querySelector(".first-paragraph").nextElementSibling;
+let firstParagraph = document.querySelector(".first-paragraph");
+let secondParagraph = firstParagraph.nextElementSibling;
 // 3. Insert the new element into the parent element
 firstParagraph.append(newAnchorElement);
 
@@ -22,11 +22,14 @@ firstParagraph.append(newAnchorElement);
 // 1. Select element to remove
 let firstH1Element = document.querySelector("H1");
 // 2. Remove it
-// firstH1Element.remove();
+firstH1Element.remove();
 // firstH1Element.parentElement.removeChild(firstH1Element); // for older browsers!!
 
 // Move existing elemnts
 let myAnchor = document.querySelector("#external-link");
 myAnchor.textContent = "sup sup sup";
-// myAnchor.parentElement.append(myAnchor);
-firstH1Element.parentElement.insertBefore(myAnchor, firstH1Element);
+myAnchor.parentElement.append(myAnchor);
+// firstH1Element.parentElement.insertBefore(myAnchor, firstH1Element);
+
+//  innerHTML
+firstParagraph.innerHTML = "This is a<strong> INNER HTML </strong>.";
