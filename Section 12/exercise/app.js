e755e6760b20e124320d1f414c6c7fe52bbc1af6
Santiago Sala <pipo0716@gmail.com>
2022-02-24 01:40:04 +0200

    279

--- Section 12/exercise/app.js
+++ Section 12/exercise/app.js
@@ -1,14 +1,19 @@
-let userInput = document.querySelector("#input-text");
-let remainingCharactersElement = document.querySelector(".count");
+const userInput = document.querySelector("#input-text");
+const remainingCharactersElement = document.querySelector(".count");
 
-let maxCharacters = userInput.getAttribute("maxlength");
+const maxCharacters = userInput.getAttribute("maxlength");
 
 userInput.addEventListener("input", updateRemainingCharacters);
 
 function updateRemainingCharacters(e) {
-  let currentUserInput = e.target.value;
-  let currentTextLenght = currentUserInput.length;
+  const currentUserInput = e.target.value;
+  const currentTextLenght = currentUserInput.length;
 
-  let remainingCharacters = maxCharacters - currentTextLenght;
+  const remainingCharacters = maxCharacters - currentTextLenght;
+
+  if (remainingCharacters < 10) {
+    remainingCharactersElement.style.color = "red";
+    userInput.style.backgroundColor = "red";
+  }
   remainingCharactersElement.textContent = remainingCharacters;
 }
