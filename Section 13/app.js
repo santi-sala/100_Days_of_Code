1ba6aa3feb97c2be85b4286d94a8f6c89d78ea57
Santiago Sala <pipo0716@gmail.com>
2022-02-25 00:57:21 +0200

    297

--- Section 13/app.js
+++ Section 13/app.js
@@ -1,3 +1,22 @@
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
+
+const users = ["Pia", "Tiago", "Julia"];
+
+// for of loops (for arrays!)
+for (const user of users) {
+  console.log(user);
+}
+
+const loggedInUser = {
+  name: "Pipo",
+  age: 32,
+  isAdmin: true,
+};
+
+// for in loops (for objects!)
+for (const propertyName in loggedInUser) {
+  console.log(propertyName);
+  console.log(loggedInUser[propertyName]);
+}
