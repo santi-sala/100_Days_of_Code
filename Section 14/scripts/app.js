99b112385c12442ed482597ddd65c0372a5dd4a9
Santiago Sala <pipo0716@gmail.com>
2022-02-26 23:40:36 +0200

    310

---
+++ Section 14/scripts/app.js
@@ -0,0 +1,5 @@
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPLayer2BtnElement = document.getElementById("edit-player-2-btn");
+
+editPlayer1BtnElemennt.addEventListener("click", openPLayerConfig);
+editPlayer2BtnElemennt.addEventListener("click", openPLayerConfig);
