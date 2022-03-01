011b2bb51de749abb9d44d6b9eafbe18c1ff48eb
Santiago Sala <pipo0716@gmail.com>
2022-03-01 15:52:43 +0200

    344

--- Section 17/app.js
+++ Section 17/app.js
@@ -1,3 +1,14 @@
-const userName = "pipo";
+const http = require("http");
 
-console.log(userName);
+function handleRequest(request, response) {
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOString() + "</h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Hello World!!</h1>");
+  }
+}
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
