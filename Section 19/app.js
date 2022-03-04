446a466810f5c4974122cf8becc9a855a1f02c8d
Santiago Sala <pipo0716@gmail.com>
2022-03-04 03:37:17 +0200

    358

--- Section 19/app.js
+++ Section 19/app.js
@@ -1,9 +1,29 @@
+const path = require("path");
+
 const express = require("express");
 
 const app = express();
 
 app.get("/", function (request, response) {
-  response.send("<h1>Hello world!!</h1>");
+  let indexFilePath = path.join(__dirname, "views", "index.html");
+  response.sendFile(indexFilePath);
+});
+
+app.get("/restaurants", function (request, response) {
+  let restaurantsFilePath = path.join(__dirname, "views", "restaurants.html");
+  response.sendFile(restaurantsFilePath);
+});
+app.get("/about", function (request, response) {
+  let aboutFilePath = path.join(__dirname, "views", "about.html");
+  response.sendFile(aboutFilePath);
+});
+app.get("/confirm", function (request, response) {
+  let confirmFilePath = path.join(__dirname, "views", "confirm.html");
+  response.sendFile(confirmFilePath);
+});
+app.get("/recommend", function (request, response) {
+  let recommendFilePath = path.join(__dirname, "views", "recommend.html");
+  response.sendFile(recommendFilePath);
 });
 
 app.listen(3000);
