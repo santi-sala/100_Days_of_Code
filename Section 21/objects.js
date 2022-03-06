64785e9a14112df1b14575df14e0ce142e291ca2
Santiago Sala <pipo0716@gmail.com>
2022-03-07 01:03:52 +0200

    392

--- Section 21/objects.js
+++ Section 21/objects.js
@@ -1,10 +1,19 @@
 class Job {
   constructor(jobTitle, place, salary) {
-    (this.title = jobTitle), (this.location = place), (this.salary = salary);
+    this.title = jobTitle;
+    this.location = place;
+    this.salary = salary;
+  }
+  describe() {
+    console.log(
+      `I'm ${this.title}, I work in ${this.location} and my salary a year is ${this.salary}€.`
+    );
   }
 }
 
 const developer = new Job("Developer", "New York", 50000);
 const cook = new Job("Cook", "Barcelona", 20000);
 console.log(developer);
+developer.describe();
 console.log(cook);
+cook.describe();
