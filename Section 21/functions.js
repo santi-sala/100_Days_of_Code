45170086b5f4f63e63572fc20f503ab41b5111db
Santiago Sala <pipo0716@gmail.com>
2022-03-06 23:46:57 +0200

    384

--- Section 21/functions.js
+++ Section 21/functions.js
@@ -4,3 +4,21 @@ function greetUser(userNAme = "user") {
 
 greetUser("pipo");
 greetUser();
+
+// Here ... is called rest parameters (Converts parameters into an array)
+function sumUp(...numbers) {
+  let result = 0;
+
+  numbers.forEach((number) => {
+    result += number;
+  });
+
+  return result;
+}
+
+const inputNumbers = [1, 6, 9, 8, 5];
+
+console.log(sumUp(1, 3, 5, 9, 8));
+
+// Here ... is called spread operator (Splits the array into its individual values as parameters)
+console.log(sumUp(...inputNumbers));
