281d2c35076a480d58ce43e9c90a7c8471067bd7
Santiago Sala <pipo0716@gmail.com>
2022-03-07 20:42:54 +0200

    395

--- Section 21/async.js
+++ Section 21/async.js
@@ -1,7 +1,12 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 
 function readFile() {
-  fs.readFile("data.txt", function (error, fileData) {
+  //   fs.readFile("data.txt", function (error, fileData) {
+  //     console.log("Joooooooooooooooo!!");
+  //     console.log(fileData.toString());
+  //   });
+
+  fs.readFile("data.txt").then(function (fileData) {
     console.log("Joooooooooooooooo!!");
     console.log(fileData.toString());
   });
