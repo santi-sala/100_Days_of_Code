e5f8e9d88d169edf77c2e27f6b6f5481b51a70db
Santiago Sala <pipo0716@gmail.com>
2022-03-14 04:03:01 +0200

    456

--- Section 26/routes/blog.js
+++ Section 26/routes/blog.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const mongodb = require("mongodb");
 
 const db = require("../data/database");
 
 const router = express.Router();
+const ObjectId = mongodb.ObjectId;
 
 router.get("/", function (req, res) {
   res.redirect("/posts");
@@ -15,6 +17,30 @@ router.get("/posts", function (req, res) {
 router.get("/new-post", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
   res.render("create-post", { authors: authors });
+});
+
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
 });
 
 module.exports = router;
