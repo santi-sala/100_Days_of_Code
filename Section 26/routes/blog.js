1f97408ea7d9d79a460ab893e109c9733740dac8
Santiago Sala <pipo0716@gmail.com>
2022-03-15 02:17:39 +0200

    459

--- Section 26/routes/blog.js
+++ Section 26/routes/blog.js
@@ -20,6 +20,19 @@ router.get("/posts", async function (req, res) {
   res.render("posts-list", { posts: posts });
 });
 
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+  if (!post) {
+    return res.status(404).render("404");
+  }
+  console.log(post);
+  res.render("post-detail", { post: post });
+});
+
 router.get("/new-post", async function (req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
   res.render("create-post", { authors: authors });
