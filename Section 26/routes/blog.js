7c5f6fa363077900de49f5144a7c3889cec74852
Santiago Sala <pipo0716@gmail.com>
2022-03-15 04:09:21 +0200

    461

--- Section 26/routes/blog.js
+++ Section 26/routes/blog.js
@@ -30,6 +30,7 @@ router.get("/posts/:id", async function (req, res) {
   if (!post) {
     return res.status(404).render("404");
   }
+
   post.humanReadableDate = post.date.toLocaleDateString("en-US", {
     weekday: "long",
     year: "numeric",
@@ -39,6 +40,40 @@ router.get("/posts/:id", async function (req, res) {
 
   post.date = post.date.toISOString();
   res.render("post-detail", { post: post });
+});
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post: post });
+});
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .updateOne(
+      { _id: new ObjectId(postId) },
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+          date: new Date(),
+        },
+      }
+    );
+
+  res.redirect("/posts");
 });
 
 router.get("/new-post", async function (req, res) {
