edd6e137c6b3f16dda5e31137036c13fb3b8e385
Santiago Sala <pipo0716@gmail.com>
2022-03-15 02:24:51 +0200

    460

--- Section 26/routes/blog.js
+++ Section 26/routes/blog.js
@@ -26,10 +26,18 @@ router.get("/posts/:id", async function (req, res) {
     .getDb()
     .collection("posts")
     .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+
   if (!post) {
     return res.status(404).render("404");
   }
-  console.log(post);
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  post.date = post.date.toISOString();
   res.render("post-detail", { post: post });
 });
 
