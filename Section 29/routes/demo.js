1bdfbd9de1ccf87d124f533195430fe75141c964
Santiago Sala <pipo0716@gmail.com>
2022-03-18 09:51:23 +0200

    492

--- Section 29/routes/demo.js
+++ Section 29/routes/demo.js
@@ -19,14 +19,36 @@ router.get("/login", function (req, res) {
 
 router.post("/signup", async function (req, res) {
   const userData = req.body;
-  const enteredmail = userData.email;
+  const enteredEmail = userData.email;
   const enteredConfirmEmail = userData["confirm-email"];
   const enteredPassword = userData.password;
+
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    console.log("Incorrect data");
+    return res.redirect("/signup");
+  }
+
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    console.log("User already exists");
+    return res.redirect("/signup");
+  }
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12);
 
   const user = {
-    email: enteredmail,
+    email: enteredEmail,
     password: hashedPassword,
   };
   await db.getDb().collection("users").insertOne(user);
@@ -36,13 +58,13 @@ router.post("/signup", async function (req, res) {
 
 router.post("/login", async function (req, res) {
   const userData = req.body;
-  const enteredmail = userData.email;
+  const enteredEmail = userData.email;
   const enteredPassword = userData.password;
 
   const existingUser = await db
     .getDb()
     .collection("users")
-    .findOne({ email: enteredmail });
+    .findOne({ email: enteredEmail });
 
   if (!existingUser) {
     console.log("User does not exist");
