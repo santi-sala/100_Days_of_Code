4afdc6d88125492bb9cc1bd293f46214e52c4719
Santiago Sala <pipo0716@gmail.com>
2022-03-22 05:48:40 +0200

    519

--- Section 31/routes/blog.js
+++ Section 31/routes/blog.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
 const mongodb = require("mongodb");
 
 const db = require("../data/database");
@@ -10,159 +9,6 @@ const router = express.Router();
 // CSRF is used here even though theres no form because the header uses csrf!! Logout has a form which sits in a header
 router.get("/", function (req, res) {
   res.render("welcome", { csrfToken: req.csrfToken() });
-});
-
-router.get("/signup", function (req, res) {
-  let sessionInputData = req.session.inputData;
-
-  //  session for handling input errors
-  if (!sessionInputData) {
-    sessionInputData = {
-      hasError: false,
-      email: "",
-      confirmEmail: "",
-      password: "",
-    };
-  }
-
-  req.session.inputData = null;
-
-  res.render("signup", {
-    inputData: sessionInputData,
-    csrfToken: req.csrfToken(),
-  });
-});
-
-router.get("/login", function (req, res) {
-  let sessionInputData = req.session.inputData;
-
-  if (!sessionInputData) {
-    sessionInputData = {
-      hasError: false,
-      email: "",
-      password: "",
-    };
-  }
-
-  req.session.inputData = null;
-  res.render("login", {
-    inputData: sessionInputData,
-    csrfToken: req.csrfToken(),
-  });
-});
-
-router.post("/signup", async function (req, res) {
-  const userData = req.body;
-  const enteredEmail = userData.email; // userData['email']
-  const enteredConfirmEmail = userData["confirm-email"];
-  const enteredPassword = userData.password;
-
-  //  Validating user input
-  if (
-    !enteredEmail ||
-    !enteredConfirmEmail ||
-    !enteredPassword ||
-    enteredPassword.trim().length < 6 ||
-    enteredEmail !== enteredConfirmEmail ||
-    !enteredEmail.includes("@")
-  ) {
-    req.session.inputData = {
-      hasError: true,
-      message: "Invalid input - please check your data.",
-      email: enteredEmail,
-      confirmEmail: enteredConfirmEmail,
-      password: enteredPassword,
-    };
-
-    req.session.save(function () {
-      res.redirect("/signup");
-    });
-    return;
-  }
-
-  const existingUser = await db
-    .getDb()
-    .collection("users")
-    .findOne({ email: enteredEmail });
-
-  // Checking if the user already exists
-  if (existingUser) {
-    req.session.inputData = {
-      hasError: true,
-      message: "User exists already!",
-      email: enteredEmail,
-      confirmEmail: enteredConfirmEmail,
-      password: enteredPassword,
-    };
-    req.session.save(function () {
-      res.redirect("/signup");
-    });
-    return;
-  }
-
-  // Hashing the password
-  const hashedPassword = await bcrypt.hash(enteredPassword, 12);
-
-  const user = {
-    email: enteredEmail,
-    password: hashedPassword,
-  };
-
-  // Saving the user to the database
-  await db.getDb().collection("users").insertOne(user);
-
-  res.redirect("/login");
-});
-
-router.post("/login", async function (req, res) {
-  const userData = req.body;
-  const enteredEmail = userData.email;
-  const enteredPassword = userData.password;
-
-  const existingUser = await db
-    .getDb()
-    .collection("users")
-    .findOne({ email: enteredEmail });
-
-  // Checking if the user is in the databse
-  if (!existingUser) {
-    req.session.inputData = {
-      hasError: true,
-      message: "Could not log you in - please check your credentials!",
-      email: enteredEmail,
-      password: enteredPassword,
-    };
-    req.session.save(function () {
-      res.redirect("/login");
-    });
-    return;
-  }
-
-  // Checking the password
-  const passwordsAreEqual = await bcrypt.compare(
-    enteredPassword,
-    existingUser.password
-  );
-
-  if (!passwordsAreEqual) {
-    req.session.inputData = {
-      hasError: true,
-      message: "Could not log you in - please check your credentials!",
-      email: enteredEmail,
-      password: enteredPassword,
-    };
-    req.session.save(function () {
-      res.redirect("/login");
-    });
-    return;
-  }
-
-  // Storing the data in the session
-  req.session.user = { id: existingUser._id, email: existingUser.email };
-  req.session.isAuthenticated = true;
-  req.session.save(function () {
-    res.redirect("/admin");
-  });
 });
 
 router.get("/admin", async function (req, res) {
@@ -190,12 +36,6 @@ router.get("/admin", async function (req, res) {
     inputData: sessionInputData,
     csrfToken: req.csrfToken(),
   });
-});
-
-router.post("/logout", function (req, res) {
-  req.session.user = null;
-  req.session.isAuthenticated = false;
-  res.redirect("/");
 });
 
 router.post("/posts", async function (req, res) {
