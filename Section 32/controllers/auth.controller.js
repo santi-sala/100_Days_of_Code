f80e07dd719c2e8949e39dcd03bc10a5a8aa8ed5
Santiago Sala <pipo0716@gmail.com>
2022-03-27 06:30:38 +0300

    538

--- Section 32/controllers/auth.controller.js
+++ Section 32/controllers/auth.controller.js
@@ -1,4 +1,7 @@
-function getSignup(req, res) {}
+function getSignup(req, res) {
+  // Remember that the path starts from views
+  res.render("customer/auth/signup");
+}
 
 function getLogin(req, res) {}
 
