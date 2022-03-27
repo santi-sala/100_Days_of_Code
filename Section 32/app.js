f7187e865afad8f0064ac3608a22d3f62ed748a6
Santiago Sala <pipo0716@gmail.com>
2022-03-27 06:15:33 +0300

    537

--- Section 32/app.js
+++ Section 32/app.js
@@ -1,9 +1,18 @@
+// Build in in node, no extra package
+const path = require("path");
+
 const express = require("express");
 
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
+//  Setting up ejs
+app.set("view engine", "ejs");
+// Constructing an absolute path to views folder
+app.set("views", path.join(__dirname, "views"));
+
+// Listening to auth.routes.js
 app.use(authRoutes);
 
 app.listen(3000);
