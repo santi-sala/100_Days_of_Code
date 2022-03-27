96fb909032c0a102654e658d12ba318b58e3b1cb
Santiago Sala <pipo0716@gmail.com>
2022-03-28 00:08:07 +0300

    545
    Adding csrf protection

--- Section 32/app.js
+++ Section 32/app.js
@@ -1,9 +1,11 @@
-// Build in in node, no extra package
+// path is a build in in node, no extra package
 const path = require("path");
 
 const express = require("express");
+const csrf = require("csurf");
 
 const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -15,9 +17,13 @@ app.set("views", path.join(__dirname, "views"));
 
 // Making the public folder availble to the final user
 app.use(express.static("public"));
-
 // Extracting values from the incomming request
 app.use(express.urlencoded({ extended: false }));
+
+// Registering for the csrf package (creates and validates the csrf tokens)
+app.use(csrf());
+// Custom middleware that distributes the csrf tokens
+app.use(addCsrfTokenMiddleware);
 
 // Listening to auth.routes.js
 app.use(authRoutes);
