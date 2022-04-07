0c808e31fcffb09f829af929913a1a8127852274
Santiago Sala <pipo0716@gmail.com>
2022-04-08 02:31:23 +0300

    574

--- Section 32/controllers/cart.controller.js
+++ Section 32/controllers/cart.controller.js
@@ -1,5 +1,24 @@
-function addCartItem(req, res) {
-  res.locals.cart.addItem();
+const Product = require("../models/product.model");
+
+async function addCartItem(req, res) {
+  let product;
+  try {
+    // req.body because its a post request
+    product = await Product.findById(req.body.productid);
+  } catch (error) {
+    next(error);
+    return;
+  }
+  const cart = res.locals.cart;
+  cart.addItem(product);
+
+  //   Updating the data on the current session!!!
+  req.session.cart = cart;
+
+  res.status(201).jason({
+    message: "Cart updated",
+    newTotalItems: cart.totalQuantity,
+  });
 }
 
 module.exports = {
--- Section 32/models/cart.model.js
+++ Section 32/models/cart.model.js
@@ -1,6 +1,8 @@
 class Cart {
-  constructor(items = []) {
+  constructor(items = [], totalQuantity = 0, totalPrice = 0) {
     this.items = items;
+    this.totalQuantity = totalQuantity;
+    this.totalPrice = totalPrice;
   }
 
   //   Storing in users (log in or not) session not in database
@@ -16,13 +18,20 @@ class Cart {
       const item = this.items[i];
       //   If theres a match, increment the quantity and total price
       if (item.product.id === product.id) {
-        cartItem.quantity = cartItem.product + 1;
-        cartItem.totalPrice = cartItem.totalPrice + product.price;
+        cartItem.quantity++;
+        cartItem.totalPrice += product.price;
         this.items[i] = cartItem;
+
+        this.totalQuantity++;
+        this.totalPrice += product.price;
         return;
       }
     }
     // If theres no match in the current cart, we add the product to the cart using the predefined cartItem
     this.items.push(cartItem);
+    this.totalQuantity++;
+    this.totalPrice += product.price;
   }
 }
+
+module.exports = Cart;
