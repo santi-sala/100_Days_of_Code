a13c125c866479720ce05c74f3930ff9ba87a2fe
Santiago Sala <pipo0716@gmail.com>
2022-04-15 01:14:51 +0300

    603

---
+++ section 34/dynamic-site/data/database.js
@@ -0,0 +1,30 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+let mongodbUrl = "mongodb://localhost:27017";
+
+// Environemnt variables
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
+}
+
+let database;
+
+async function initDatabase() {
+  const client = await MongoClient.connect(mongodbUrl);
+  database = client.db("deployment");
+}
+
+function getDb() {
+  if (!database) {
+    throw new Error("No database connected!");
+  }
+
+  return database;
+}
+
+module.exports = {
+  initDatabase: initDatabase,
+  getDb: getDb,
+};
